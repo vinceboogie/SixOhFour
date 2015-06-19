de12c46bf5876e3fae995511be1514d2678b624f
Jem Gallego <jngallego@gmail.com>
2015-06-18 18:57:59 -0700

    Test commit - Jem

--- SixOhFour/ViewController.swift
+++ SixOhFour/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         println("Test commit.")
+        println("Can you see my commit? - Jem")
         
     }
 
