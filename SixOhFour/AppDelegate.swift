8bda2eb7dcca6fe8316156cd3254d68a253cb236
Jem <jemsomnia@gmail.com>
2015-06-19 01:18:18 -0700

    Created the main tab navigation

--- SixOhFour/AppDelegate.swift
+++ SixOhFour/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+            
+        UITabBar.appearance().barTintColor = UIColor.whiteColor()
+        
+        
+            
         return true
     }
 
@@ -49,7 +54,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "com.vinceboogie.SixOhFour" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1] as! NSURL
+        return urls[urls.count-1] as NSURL
     }()
 
     lazy var managedObjectModel: NSManagedObjectModel = {
--- SixOhFour/ViewController.swift
+++ SixOhFour/ViewController.swift
@@ -13,9 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        println("TEST")
-        
+   
     }
 
     override func didReceiveMemoryWarning() {
