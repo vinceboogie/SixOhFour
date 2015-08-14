4d1bad7c406bf97e1a659a3d6fe184983cc97e2b
Joseph Pelina <joseph.pelina@gmail.com>
2015-08-14 01:33:11 -0700

    Finished for the day. added 2 new VCs = ManualEdit and AddNewShift.

---
+++ SixOhFour/ManualEditsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ManualEditsListTableViewCell.swift
+//  SixOhFour
+//
+//  Created by Joseph Pelina on 8/13/15.
+//  Copyright (c) 2015 vinceboogie. All rights reserved.
+//
+
+import UIKit
+
+class ManualEditsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
